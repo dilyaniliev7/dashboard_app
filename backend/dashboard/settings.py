629025a5aae56209d0f5c5d49ef766d3407e7bf3
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-02-12 20:03:35 +0200

    setting up react js frontend

--- backend/dashboard/settings.py
+++ backend/dashboard/settings.py
@@ -22,12 +22,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'data',
+    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -65,6 +67,9 @@ DATABASES = {
     }
 }
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
