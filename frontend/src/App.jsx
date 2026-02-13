2139573a69a9c39af8e664e510b0a06d016e22cc
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-02-13 21:59:16 +0200

    page navigation and navbar added with React Router Dom & Material UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Dashboard1 from './components/Dashboard1'
+import Dashboard2 from './components/Dashboard2'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar
+        content = {
+            <Routes>
+              <Route path="" element={<Dashboard1/>}/>
+              <Route path="/dashboard2" element={<Dashboard2/>}/>
+          </Routes>
+            }
+      />
+
     </>
   )
 }
