a6b0178412faba07e0e9fe3f9ec1ec3d1d9f7f0e
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-02-18 19:20:49 +0200

    styling the chart

--- frontend/src/components/charts/ChartBox.jsx
+++ frontend/src/components/charts/ChartBox.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Grid, Box } from '@mui/material';
 
 export default function MyChartBox(props) {
-  const {icon1, title1, chart1} = props
+  const {icon1, title1, chart1, icon2, title2, chart2, icon3, title3, chart3} = props
   return (
     <>
         <Grid container
@@ -10,30 +10,40 @@ export default function MyChartBox(props) {
         >
             <Grid
                 item xs={12} sm={12} md={6} lg={4}
-                sx={{minHeight: '200px', padding: '20px', backgroundColor:'red'}}
+                sx={{minHeight: '200px', padding: '20px', borderRight: '1px dotted #d3d3d3'}}
             >
-                <Box>
-                    <Box>{icon1}</Box>
+                <Box sx={{marginBottom: '20px', fontWeight:'bold', display:'flex', flexDirection:'row', alignItems:'center'}}>
+                    <Box sx={{marginRight: '15px'}}>{icon1}</Box>
                     <Box>{title1}</Box>
                 </Box>
 
-                <Box>{chart1}</Box>
+                <Box  sx={{marginBottom: '20px'}}>{chart1}</Box>
 
             </Grid>
 
             <Grid
                 item xs={12} sm={12} md={6} lg={4}
-                sx={{minHeight: '200px', padding: '20px', backgroundColor:'blue'}}
+                sx={{minHeight: '200px', padding: '20px', borderRight: '1px dotted #d3d3d3'}}
             >
+                <Box sx={{marginBottom: '20px', fontWeight:'bold', display:'flex', flexDirection:'row', alignItems:'center'}}>
+                    <Box sx={{marginRight: '15px'}}>{icon2}</Box>
+                    <Box>{title2}</Box>
+                </Box>
 
+                <Box  sx={{marginBottom: '20px'}}>{chart2}</Box>
 
             </Grid>
 
             <Grid
                 item xs={12} sm={12} md={6} lg={4}
-                sx={{minHeight: '200px', padding: '20px', backgroundColor:'purple'}}
+                sx={{minHeight: '200px', padding: '20px', borderRight: '1px dotted #d3d3d3'}}
             >
+                <Box sx={{marginBottom: '20px', fontWeight:'bold', display:'flex', flexDirection:'row', alignItems:'center'}}>
+                    <Box sx={{marginRight: '15px'}}>{icon3}</Box>
+                    <Box>{title3}</Box>
+                </Box>
 
+                <Box  sx={{marginBottom: '20px'}}>{chart3}</Box>
 
             </Grid>
         </Grid>
